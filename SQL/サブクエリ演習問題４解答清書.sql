eb117dd7e53a83495471c56fc5a17093c7e7344b
yksk96 <yokosuka0609@gmail.com>
2025-10-04 08:43:15 +0900

    Update サブクエリ演習問題４解答清書.sql

--- SQL/サブクエリ演習問題４解答清書.sql
+++ SQL/サブクエリ演習問題４解答清書.sql
@@ -13,21 +13,27 @@ select
   su from meisai4 m4 
 where (select name from syouhin4 s4 where s4.sno = m4.sno) = 'A菓子'
 ; 
---別解(暗黙的にINNER JOINと同じに扱われる)
-SELECT 
-    s4.name, -- SELECT句がとてもシンプルになる
-    t4.name,
-    h4.day,
-    m4.su
-FROM
-    meisai4 m4, -- 使うテーブルを全て並べる
-    hanbai4 h4,
-    syouhin4 s4,
-    tokuisaki4 t4
-WHERE
-    -- ここでテーブル間の「相関関係」を全て記述する
-    m4.hno = h4.hno
-AND m4.sno = s4.sno
-AND h4.tno = t4.tno
--- そして、絞り込み条件もここに書く
-AND s4.name = 'A菓子';  
+-- ⑵最新の船橋商会の売上日の販売実績
+-- 得意先、販売番号、売上日、商品名、単価、売上額（販売番号、商品番号の昇順で表示）を表示せよ。
+-- 自力チャレンジ
+テーブルHANBAI4……HNO販売番号,DAY売上日,TNO得意先番号
+テーブルMEISAI4……MNO明細番号,HNO販売番号,SNO商品番号,SU売上数量
+テーブルSYOUHIN4……SNO商品番号,NAME商品名,TANKA単価
+テーブルTOKUISAKI4……TNO得意先番号,NAME得意先名,ADR住所
+select 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
