a51828d02abeb905b50458e20dee879446b66468
yksk96 <yokosuka0609@gmail.com>
2025-10-04 08:39:13 +0900

    Update サブクエリ演習問題４解答清書.sql

--- SQL/サブクエリ演習問題４解答清書.sql
+++ SQL/サブクエリ演習問題４解答清書.sql
@@ -1,1 +1,16 @@
+--　1004ごちゃごちゃしてしまったので、こちらに見ないで書くチャレンジしつつ、解答書き直し
+- 演習問題４
+テーブルHANBAI4……HNO販売番号,DAY売上日,TNO得意先番号
+テーブルMEISAI4……MNO明細番号,HNO販売番号,SNO商品番号,SU売上数量
+テーブルSYOUHIN4……SNO商品番号,NAME商品名,TANKA単価
+テーブルTOKUISAKI4……TNO得意先番号,NAME得意先名,ADR住所
 
+-- ⑴商品A菓子の販売実績、商品名、得意先名、販売日付、売上数を表示(得意先ﾃｰﾌﾞﾙと商品ﾃｰﾌﾞﾙは結合するのはNG)
+select 
+  (select name from syouhin4 s4 where s4.sno = m4.sno) as　商品名,
+  (select tno from tokuisaki4 t4 where t4.tno = (select tno from hanbai4 h4 where h4.hno = m4.hno )) as 得意先名,
+  (select day from hanbai4 h4 where h4.hno = m4.hno)as 販売日付,
+  su from meisai4 m4 
+where (select name from syouhin4 s4 where s4.sno = m4.sno) = 'A菓子'
+  ; 
+  
