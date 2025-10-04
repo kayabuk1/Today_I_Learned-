ac3e30f9ba073ef0808619cbc54af3f160405e70
yksk96 <yokosuka0609@gmail.com>
2025-10-04 12:49:11 +0900

    Update サブクエリ演習問題４解答清書.sql

--- SQL/サブクエリ演習問題４解答清書.sql
+++ SQL/サブクエリ演習問題４解答清書.sql
@@ -67,8 +67,26 @@ AND (select sum(m4.su *(select s4.tanka from syouhin4 s4 where s4.sno = m4.sno))
       where m4.hno IN (select h4.hno from hanbai4 h4 where h4.tno =(select tno from tokuisaki4 where name = '鎌ヶ谷商会')))
 ;
 
---⑹千葉商会
-
+--⑹千葉商会への売上について、商品ごとの売り上げ合計数が習志野・鎌ヶ谷商会の売上合計数を超えた商品
+select 
+  (select t4.name from tokuisaki4 t4 where t4.tno = (select h4.tno from hanbai4 h4 where h4.hno = (select m4.hno from meisai4 m4 where m4.sno = s4.sno)))as 得意先名,
+  s4.sno as 商品番号, s4.name as 商品名, 
+  (select sum(m4.su) from meisai4 m4 where m4.sno = s4.sno))as 売上合計数
+from syouhin4 s4 
+where (select sum(m4.su) from meisai4 m4 where m4.sno = s4.sno)) > ANY
+       (select sum(m4.su) from meisai4 m4 where m4.sno = 
+          (select s4.sno from syouhin4 s4 where 
+            (s4.sno = 
+              (select m4.sno from meisai4 m4 where m4.hno = 
+                (select h4.hno from hanbai4 where h4.tno = 
+                  (select t4.tno from tokuisaki4 t4 where t4.name = '習志野商会' OR '鎌ヶ谷商会' 
+                  )
+                )
+              )
+            )
+          )
+        )
+  ;
 
 
 
