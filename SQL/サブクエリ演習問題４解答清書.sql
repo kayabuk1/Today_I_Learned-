6adc2f7d182c7b04327ecf15692dca7f1d1570f6
yksk96 <yokosuka0609@gmail.com>
2025-10-04 09:13:29 +0900

    Update サブクエリ演習問題４解答清書.sql

--- SQL/サブクエリ演習問題４解答清書.sql
+++ SQL/サブクエリ演習問題４解答清書.sql
@@ -20,7 +20,9 @@ where (select name from syouhin4 s4 where s4.sno = m4.sno) = 'A菓子'
 テーブルMEISAI4……MNO明細番号,HNO販売番号,SNO商品番号,SU売上数量
 テーブルSYOUHIN4……SNO商品番号,NAME商品名,TANKA単価
 テーブルTOKUISAKI4……TNO得意先番号,NAME得意先名,ADR住所
-select (select tno from tokuisaki4 t4 where )
+select
+  (select name from tokuisaki4 t4 where t4.tno = (select tno from hanbai4 h4 where h4.hno = m4.hno))as 得意先名,
+  
 from meisai4 m4
 where (select name from tokuisaki4 t4 where t4.tno = (select tno from hanbai4 h4 where h4.hno = m4.hno ) = '船橋商会'
 order by m4.hno and s4.sno ASC; 
